6a0495330c5aed3d9a84aa45bdc7887ae6d3b4ae
Khattri-Mahesh <meshkhatt1@gmail.com>
2024-12-02 15:16:33 +0000

    Update and rename Map test.py to Map Making.py

--- Map Making.py
+++ Map Making.py
@@ -5,18 +5,6 @@ import webbrowser
 center_coordinates = [0, -0]
 my_map = folium.Map(location=center_coordinates, zoom_start=4)
 
-
-# adding markers test
-
-folium.Marker(
-    location=[51, -0],  # london co ordinates
-    popup="London",
-    icon=folium.Icon(color="red", icon="sign")
-).add_to(my_map)
-
-
-
-
 # save map in a html file
 map_file = "map.html"
 my_map.save(map_file)
